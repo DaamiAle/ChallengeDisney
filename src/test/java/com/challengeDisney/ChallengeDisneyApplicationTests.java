6fe2f0f097175839c6078fa8a733e1014b2bed7a
DaamiAle <daamiale.recoil@gmail.com>
2021-07-21 21:15:07 -0300

    Initial commit

---
+++ src/test/java/com/challengeDisney/ChallengeDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeDisney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
