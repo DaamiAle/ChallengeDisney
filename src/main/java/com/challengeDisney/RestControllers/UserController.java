bf897873fa658984c2348ddb88d1c351fba99182
DaamiAle <daamiale.recoil@gmail.com>
2021-08-26 01:01:56 -0300

    Fix class UserController

--- src/main/java/com/challengeDisney/RestControllers/UserController.java
+++ src/main/java/com/challengeDisney/RestControllers/UserController.java
@@ -1,22 +1,12 @@
 package com.challengeDisney.RestControllers;
 
-//import java.util.List;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//import com.challengeDisney.Models.UserModel;
-
-//import com.challengeDisney.Services.UserServiceInterface;
-//
-//import lombok.RequiredArgsConstructor;
 
 import com.challengeDisney.Services.UserService;
 
 import lombok.RequiredArgsConstructor;
-//import com.challengeDisney.Services.UserServiceDetails;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,13 +14,9 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	
 	private final UserService userServ;
-	
-	
-	
-	
-//	
-//	@GetMapping
-//	public String userPage() {
-//		return "ESTA ES LA PAGINA DE USUARIOS";
-//	}
+
+	@GetMapping
+	public String userPage() {
+		return "ESTA ES LA PAGINA DE USUARIOS";
+	}
 }
